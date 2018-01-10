56b026bff18288250eb83c6deeec423c3a3111b0
thurt <taylor.a.hurt@gmail.com>
2018-01-10 00:25:18 +0000

    adds basic form to Setup

--- src/components/Setup.tsx
+++ src/components/Setup.tsx
@@ -1,7 +1,40 @@
 import * as React from "react";
 
 export class Setup extends React.Component<{}, {}> {
+    constructor(props: {}) {
+        super(props)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+
+    handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault()
+        const id = event.currentTarget.elements.namedItem("id")
+        if (!(id instanceof HTMLInputElement)) {
+            throw new Error("want form element named \"id\" to be instanceof HTMLInputElement")
+        }
+        const password = event.currentTarget.elements.namedItem("password")
+        if (!(password instanceof HTMLInputElement)) {
+            throw new Error("want form element named \"password\" to be instanceof HTMLInputElement")
+        }
+        alert("Your submission\n" + id.value + ":" + password.value)
+    }
+
     render() {
-        return (<div>Setup</div>)
+        return (
+            <div>
+                <h2>Setup Admin account</h2>
+                <p>{"Since this is your first time accessing your blog, you will first need to create an admin account. Please choose your id and password."}</p>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Id: <input name="id" type="text" />
+                    </label>
+                    <label>
+                        Password: <input name="password" type="password" />
+                    </label>
+                    <input type="submit" value="Create Account" />
+                </form>
+            </div>
+        )
     }
 }
