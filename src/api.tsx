e39db806b42543693b635dee60826d86971321e0
thurt <taylor.a.hurt@gmail.com>
2018-01-11 16:46:00 +0000

    add comment for apiError

--- src/api.tsx
+++ src/api.tsx
@@ -5,6 +5,7 @@ const request = swaggerClient.CmsApiFactory(
   'http://172.17.0.1:8282',
 );
 
+// apiError is the interface returned by the api in the response body when a request error has occurred. Common examples of request errors that would cause the server to respond with an apiError would be when the the request contains invalid or missing values, or when a request is made for a non-existant entity.
 interface apiError {
   error: string;
   code: number;
