5682db68437d757fa8e612451807170e12411d63
thurt <taylor.a.hurt@gmail.com>
2018-01-12 03:17:29 +0000

    changes api base url

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import * as swaggerClient from 'cms-client-api';
 
 export const request = swaggerClient.CmsApiFactory(
   undefined,
-  'http://172.17.0.1:8282',
+  'https://localhost:8000/api',
 );
 
 // apiError is the interface returned by the api in the response body when a request error has occurred. Common examples of request errors that would cause the server to respond with an apiError would be when the the request contains invalid or missing values, or when a request is made for a non-existant entity.
