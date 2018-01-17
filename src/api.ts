78f2423a825be9ecb3d1da48de46d99c2f5078ad
thurt <taylor.a.hurt@gmail.com>
2018-01-17 18:38:22 +0000

    changes api host address to use same domain on which client is delivered

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,6 @@
 import * as api from 'cms-client-api';
 
-export const request = api.CmsApiFactory(
-  undefined,
-  'https://localhost:8000/api',
-);
+export const request = api.CmsApiFactory(undefined, '/api');
 
 // apiError is the interface returned by the api in the response body when a request error has occurred. Common examples of request errors that would cause the server to respond with an apiError would be when the the request contains invalid or missing values, or when a request is made for a non-existant entity.
 interface apiError {
