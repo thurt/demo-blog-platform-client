805fe7c0e53c7458ef714289c5eda18ef0f05aa6
thurt <taylor.a.hurt@gmail.com>
2018-01-20 22:56:22 +0000

    adds basic ndjson functionality for retrieving Posts from api

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,6 +1,47 @@
 import * as React from 'react';
+import * as ndjsonStream from 'can-ndjson-stream';
+import * as api from '../api';
+import {CmsPost} from 'cms-client-api';
+
+type postChunk = {
+  done: boolean;
+  value: CmsPost;
+};
 
 export class Posts extends React.Component<{}, {}> {
+  static onReceivePost(r: ReadableStream) {
+    return (p: postChunk) => {
+      if (p.done) {
+        console.log('stream complete');
+        return;
+      }
+      console.log('got value:', p.value);
+      r
+        .getReader()
+        .read()
+        .then(Posts.onReceivePost(r));
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const r = await fetch(api.basePath + '/posts');
+      if (!r.ok) {
+        throw r;
+      }
+
+      const postStream = ndjsonStream(r.body).getReader();
+
+      let result: postChunk;
+      while (!result || !result.done) {
+        result = await postStream.read();
+        console.log(result.done, result.value);
+      }
+    } catch (e) {
+      api.handleError(e);
+    }
+  }
+
   render() {
     return <div>Posts</div>;
   }
