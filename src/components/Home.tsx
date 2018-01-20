3a00c9d4a00bb114093ff7c9251dc430f85c2603
thurt <taylor.a.hurt@gmail.com>
2018-01-20 16:12:48 +0000

    adds ref link for issue and flips predicate in Home render()

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,6 +9,7 @@ export class Home extends React.Component<{}, {}> {
         const isSetup = await api.request.isSetup();
         // using type guard to notify tsc that isSetup is actually a boolean even though isSetup() is typed to return "{value: boolean}".
         // there is a bug in the server package grpc-gateway that does not handle well-known types from protobuf/wrappers.proto properly
+        // https://github.com/grpc-ecosystem/grpc-gateway/pull/412
         if (typeof isSetup !== 'boolean') {
           api.handleError(
             new Error(
@@ -30,13 +31,13 @@ export class Home extends React.Component<{}, {}> {
   render() {
     return (
       <div>
-        {window.app.state.isSetup === undefined ? (
-          <em>Loading...</em>
-        ) : (
+        {window.app.state.isSetup === true ? (
           <div>
             <h4>Homepage</h4>
             <LoginStatus />
           </div>
+        ) : (
+          <em>Loading...</em>
         )}
       </div>
     );
