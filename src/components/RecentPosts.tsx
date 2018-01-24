688fb0a7e4ee8301ec2a8d33d2a6ab29a2784bb5
thurt <taylor.a.hurt@gmail.com>
2018-01-24 16:13:08 +0000

    changes initial posts state to undefined, which signals Loading status

--- src/components/RecentPosts.tsx
+++ src/components/RecentPosts.tsx
@@ -15,7 +15,7 @@ type State = {
 export class RecentPosts extends React.Component<{}, State> {
   constructor(props: {}) {
     super(props);
-    this.state = {posts: []};
+    this.state = {posts: undefined};
   }
 
   async componentDidMount() {
@@ -34,30 +34,35 @@ export class RecentPosts extends React.Component<{}, State> {
           break;
         }
 
-        this.setState({posts: this.state.posts.concat(pc.value.result)});
+        this.setState({
+          posts: (this.state.posts || []).concat(pc.value.result),
+        });
       }
+      // will be true when have finished fetching posts but there were no posts
+      if (this.state.posts === undefined) this.setState({posts: []});
     } catch (e) {
       api.handleError(e);
     }
   }
 
   render() {
+    const ps = this.state.posts;
     return (
       <div>
         <h3>Recent Posts</h3>
-        {this.state.posts.length === 0 ? (
-          <em>This blog has no posts yet!</em>
-        ) : null}
-        {this.state.posts.map((p, i) => {
-          return (
-            <div key={p.id}>
-              <h4>
-                <a href={`/posts/${p.slug}`}>{p.title || 'Untitled'}</a>
-              </h4>
-              <p>{p.created}</p>
-            </div>
-          );
-        })}
+        {ps === undefined ? <em>Loading...</em> : null}
+        {ps && ps.length === 0 ? <em>This blog has no posts yet!</em> : null}
+        {ps &&
+          ps.map((p, i) => {
+            return (
+              <div key={p.id}>
+                <h4>
+                  <a href={`/posts/${p.slug}`}>{p.title || 'Untitled'}</a>
+                </h4>
+                <p>{p.created}</p>
+              </div>
+            );
+          })}
       </div>
     );
   }
