4ca2234475bac035ae7a417eb895517bd0e55362
thurt <taylor.a.hurt@gmail.com>
2018-01-27 21:37:23 +0000

    removes unused import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Store from './store';
 import {Main} from './components/Main';
 
 ReactDOM.render(<Main />, document.getElementById('main-wrapper'));
