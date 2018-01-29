8a9ff701d134db750b4e4fee2fe44fd179ec62ac
thurt <taylor.a.hurt@gmail.com>
2018-01-29 06:30:43 +0000

    updates Editor component iframe src and adds deploy/editor/ to git

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,11 +2,6 @@ import * as React from 'react';
 
 export class Editor extends React.Component<{}, {}> {
   render() {
-    return (
-      <iframe
-        style={{border: 0, flex: 1}}
-        src="https://rawgit.com/thurt/demo-mvvm-note-app/master/dist/index.html"
-      />
-    );
+    return <iframe style={{border: 0, flex: 1}} src="/editor/index.html" />;
   }
 }
