8a70b4bd1f186da207b03e7fe956c94a77529267
thurt <taylor.a.hurt@gmail.com>
2018-02-02 16:36:02 +0000

    adds MyDate class which sets 2-digit display options so dates are set number of chars

--- src/date.ts
+++ src/date.ts
@@ -1,4 +1,27 @@
 // expects dateString format like '2018-02-01 22:37:20'
 export function GMT(dateString: string): Date {
-  return new Date(`${dateString} GMT`);
+  // adding GMT at the end of the dateString causes Date constructor to interpret timezone as GMT
+  return new MyDate(`${dateString} GMT`);
+}
+
+class MyDate extends Date {
+  static options: Intl.DateTimeFormatOptions = {
+    month: '2-digit',
+    day: '2-digit',
+    year: 'numeric',
+  };
+  constructor(dateString: string) {
+    super(dateString);
+  }
+  toLocaleString() {
+    return super.toLocaleString(undefined, MyDate.options);
+  }
+
+  toLocaleDateString() {
+    return super.toLocaleDateString(undefined, MyDate.options);
+  }
+
+  toLocaleTimeString() {
+    return super.toLocaleTimeString(undefined, MyDate.options);
+  }
 }
