dd1247b1a85b030677fe714175b5b24b708efa1e
thurt <taylor.a.hurt@gmail.com>
2018-02-03 21:18:10 +0000

    installs nprogress and sets up naive proxy handling controls

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,56 @@
 import * as api from 'cms-client-api';
 import ndjsonStream = require('can-ndjson-stream'); // This file should be imported using the CommonJS-style
+import * as nprogress from 'nprogress';
 
+let activeReqs = 0;
+const ph: ProxyHandler<
+  api.PostsApi | api.UsersApi | api.AuthApi | api.CommentsApi | api.SetupApi
+> = {
+  get(target, name, receiver) {
+    return (...args: any[]) => {
+      if (activeReqs++ === 0 && !nprogress.isStarted()) {
+        nprogress.start();
+      }
+      //@ts-ignore
+      return target[name](...args)
+        .then((_: any) => {
+          if (--activeReqs === 0) {
+            nprogress.done();
+          } else {
+            nprogress.inc();
+          }
+          return _;
+        })
+        .catch((_: any) => {
+          if (--activeReqs === 0) {
+            nprogress.remove();
+          }
+          return Promise.reject(_);
+        });
+    };
+  },
+};
 export const basePath = '/api';
-export const posts = new api.PostsApi(undefined, basePath);
-export const users = new api.UsersApi(undefined, basePath);
-export const auth = new api.AuthApi(undefined, basePath);
-export const comments = new api.CommentsApi(undefined, basePath);
-export const setup = new api.SetupApi(undefined, basePath);
+export const posts = new Proxy(
+  new api.PostsApi(undefined, basePath),
+  ph,
+) as api.PostsApi;
+export const users = new Proxy(
+  new api.UsersApi(undefined, basePath),
+  ph,
+) as api.UsersApi;
+export const auth = new Proxy(
+  new api.AuthApi(undefined, basePath),
+  ph,
+) as api.AuthApi;
+export const comments = new Proxy(
+  new api.CommentsApi(undefined, basePath),
+  ph,
+) as api.CommentsApi;
+export const setup = new Proxy(
+  new api.SetupApi(undefined, basePath),
+  ph,
+) as api.SetupApi;
 
 // Error is the interface returned by the api in the response body when a request error has occurred. Common examples of request errors that would cause the server to respond with an Error would be when the the request contains invalid or missing values, or when a request is made for a non-existant entity.
 export interface Error {
@@ -20,6 +64,10 @@ type chunk = {
 };
 
 export async function streamRequest(path: string, cb: (c: chunk) => void) {
+  if (activeReqs++ === 0 && !nprogress.isStarted()) {
+    nprogress.start();
+  }
+
   const r = await fetch(path);
   if (!r.ok) {
     throw r;
@@ -29,10 +77,14 @@ export async function streamRequest(path: string, cb: (c: chunk) => void) {
 
   let c: chunk;
   while (true) {
-    c = await reader.read();
+    c = await reader.read(); // i think this could throw; need to try catch here so can handle the nprogress for streamRequests
     if (c.done) {
+      if (--activeReqs === 0) {
+        nprogress.done();
+      }
       break;
     }
+    nprogress.inc();
     cb(c);
   }
 }
