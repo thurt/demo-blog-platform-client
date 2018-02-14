9bf1f336bd8c71c0a423942ad45b7169fab11d94
thurt <taylor.a.hurt@gmail.com>
2018-02-14 17:51:17 +0000

    updates Editor to overwrite iframe Error and fetch objects with parent versions

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,32 @@
 import * as React from 'react';
+import * as error from '../error';
+
+declare global {
+  interface Window {
+    errorHandler: typeof error.Handle;
+    Error: Error['constructor'];
+  }
+}
 
 export class Editor extends React.Component<{}, {}> {
+  shouldComponentUpdate() {
+    return false;
+  }
   render() {
-    return <iframe style={{border: 0, flex: 1}} src="/editor/app.html" />;
+    return (
+      <iframe
+        style={{border: 0, flex: 1}}
+        src="/editor/app.html"
+        ref={iframe => {
+          if (iframe) {
+            iframe.contentWindow.errorHandler = error.Handle;
+            // overwriting iframe window constructors with parent constructors.
+            // see notes on "multiple contexts" https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
+            iframe.contentWindow.Error = Error;
+            iframe.contentWindow.fetch = fetch;
+          }
+        }}
+      />
+    );
   }
 }
